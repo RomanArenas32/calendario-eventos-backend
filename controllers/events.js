fdf0669c6251c5057cb7a3fef85b982bddac6880
Roman Arenas <roman.ea3232@gmail.com>
2023-04-10 02:41:56 -0300

    Add CRUD back

--- controllers/events.js
+++ controllers/events.js
@@ -1,30 +1,123 @@
 
-const {response} = require('express');
+const { response } = require('express');
+const Evento = require('../models/Evento');
 
-const getEvento = (req, res = response)=>{
+const getEvento = async (req, res = response) => {
+
+    const eventos = await Evento.find().populate('user', 'name');
+
+
     return res.status(200).json({
         ok: true,
-        msg: 'Obteniendo evento'
+        eventos
     })
 }
-const crearEvento = (req, res = response)=>{
-    return res.status(200).json({
-        ok: true,
-        msg: 'Creando evento'
-    })
+const crearEvento = async (req, res = response) => {
+
+    const evento = new Evento(req.body);
+
+    try {
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save();
+
+        res.status(200).json({
+            ok: true,
+            evento: eventoGuardado
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al concectar a la base de datos reintente nuevamente. Si el error persiste comuniquese con el administrador'
+        })
+    }
+
 }
 
-const actualizarEvento = (req, res = response)=>{
-    return res.status(200).json({
-        ok: true,
-        msg: 'Actualizando evento'
-    })
+const actualizarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El evento que intenta acceder es inexistente'
+            });
+        }
+        if (evento.user.toString() != uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'El evento solo puede modificarse por el usuario que lo creo'
+            });
+
+
+        }
+        //succes update
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, { new: true }); //este ultimo argumento implica que solo se mostrara el evento actualizado y no los dos
+        console.log(eventoActualizado);
+
+        return res.status(200).json({
+            ok: true,
+            evento: eventoActualizado
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error al conectar con la base de datos. Reintente nuevamente. Si el error persiste comuniquese con el administrados'
+        });
+    }
+
 }
 
-const borrarEvento = (req, res = response)=>{
-    return res.status(200).json({
+const borrarEvento = async (req, res = response) => {
+
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El evento que intenta acceder es inexistente'
+            });
+        }
+        if (evento.user.toString() != uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'El evento solo puede eliminarse por el usuario que lo creo'
+            });
+        }
+        await Evento.findByIdAndDelete(evento);
+        return res.json({ ok: true })
+    }
+
+
+    catch (error) {
+        console.log(error)
+    }
+    return res.status(500).json({
         ok: true,
-        msg: 'Borrando evento'
+        msg: 'Error en el servidor. Por favor hable con el administrador'
     })
 }
 
--- routes/events.js
+++ routes/events.js
@@ -3,25 +3,44 @@
 */
 
 const { getEvento, crearEvento, actualizarEvento, borrarEvento } = require("../controllers/events");
+const { check } = require('express-validator');
 const { Router } = require('express');
 const router = Router();
 const validarJwt = require('../middlewares/validar-jwt');
+const validarCampos = require('../middlewares/validar-campos');
+const isDate = require("../helpers/isDate");
 
+router.use(validarJwt); //shared midleware in all routes
 
 
 //events
 
 //get
-router.get('/', validarJwt, getEvento);
+router.get('/', getEvento);
 
 //create
-router.post('/', validarJwt, crearEvento);
+router.post('/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'La fecha es obligatoria').custom(isDate),
+
+        validarCampos
+
+    ]
+    , crearEvento);
 
 //update
-router.put('/:id', validarJwt, actualizarEvento);
+router.put('/:id',
+    [check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'La fecha es obligatoria').custom(isDate), 
+    
+    validarCampos
+
+]
+    , actualizarEvento);
 
 //delete
-router.delete('/:id', validarJwt, borrarEvento);
+router.delete('/:id', borrarEvento);
 
 
 module.exports = router;
